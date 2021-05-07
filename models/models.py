a19558b5f35b71bf5d6ed811621481bf24c1d140
George <georgematheuslima@outlook.com>
2021-05-06 23:45:21 -0300

    adicionando novas funções ao menu

--- models/models.py
+++ models/models.py
@@ -1,11 +1,12 @@
 
 class Pet:
     def __init__(self,nome, raca, porte, dono, id=None):
+        self.id = id
         self.nome = nome
         self.raca = raca
         self.porte = porte
         self.dono = dono
-        self._id = id
+
 
 
 class Usuario:
